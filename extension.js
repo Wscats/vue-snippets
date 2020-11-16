9849da2f9c25c3548159b5cfaeae83ba78d0a36f
enoyao <enoyao@tencent.com>
2020-11-16 09:58:14 +0800

    更新格式化
    更新格式化

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require("vscode");
 const statusBarUi = require("./status");
-const format = require('./format');
+const prettier = require("prettier");
 
 function activate(context) {
     let compileOff = vscode.commands.registerCommand("vue3snippets.compileOff", () => {
@@ -15,13 +15,26 @@ function activate(context) {
     });
     context.subscriptions.push(compileOn);
     context.subscriptions.push(compileOff);
-    vscode.workspace.onWillSaveTextDocument(() => {
+    vscode.workspace.onWillSaveTextDocument(({ document }) => {
         let config = vscode.workspace.getConfiguration("vue3snippets");
         let isEnableOnDidSaveTextDocument = config.get("enable-compile-vue-file-on-did-save-code");
         if (!isEnableOnDidSaveTextDocument) { return };
         let activeTextEditor = vscode.window.activeTextEditor;
         if (activeTextEditor && activeTextEditor.document.languageId === 'vue') {
-            format.init();
+            const filepath = document.uri.fsPath;
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) throw new Error('no active editor');
+            const doc = editor.document;
+            const lineCount = doc.lineCount;
+            const text = doc.getText();
+            const start = new vscode.Position(0, 0);
+            const end = new vscode.Position(lineCount + 1, 0);
+            const range = new vscode.Range(start, end);
+            const prettierText = prettier.format(text, { filepath });
+            editor.edit((editBuilder, error) => {
+                error && window.showErrorMessage(error);
+                editBuilder.replace(range, prettierText);
+            });
         } else {
             // vscode.window.showInformationMessage('It‘s not a .vue file');
         }
