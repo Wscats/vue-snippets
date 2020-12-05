29cd536a83e4dc7cd65d6e11a5d190bc4fedeecb
wscats <kalone.cool@gmail.com>
2020-12-05 17:11:16 +0800

    修复注释错误问题
    修复注释错误问题

--- tests/test.vue
+++ tests/test.vue
@@ -1,3 +1,43 @@
 <template>
-<p>123</p>
+  <header
+    :style="{
+      backgroundColor: color ? color : defaultColor,
+    }"
+  >
+    {{ title }}
+  </header>
+  <a href="123"> 1 </a>
 </template>
+<script>
+import { reactive } from "@vue/composition-api";
+export default {
+  // 父组件传递进来更改该头部组件的属性值
+  props: {
+    // 标题
+    title: String,
+    // 颜色
+    color: String,
+  },
+  setup() {
+    const state = reactive({
+      defaultColor: "red",
+    });
+    return {
+      ...state,
+    };
+  },
+};
+</script>
+<style scoped>
+p {
+  color: red;
+}
+header {
+  color: red;
+  height: 50px;
+  width: 100%;
+  line-height: 50px;
+  text-align: center;
+  color: white;
+}
+</style>
