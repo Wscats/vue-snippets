33274078654c98572999efc3d5e34a909271d117
wscats <kalone.cool@gmail.com>
2020-12-06 10:23:40 +0800

    Update test.vue

--- tests/test.vue
+++ tests/test.vue
@@ -7,9 +7,16 @@
     {{ title }}
   </header>
   <a href="123"> 1 </a>
+  <a href="123"> 1 </a>
+  <a href="123"> 1 </a>
+  <a href="123"> 1 </a><a href="123"> 1 </a><a href="123"> 1 </a> <a href="123"> 1 </a
+  ><a href="123"> 1 </a><a href="123"> 1 </a> <a href="123"> 1 </a><a href="123"> 1 </a
+  ><a href="123"> 1 </a> <a href="123"> 1 </a><a href="123"> 1 </a><a href="123"> 1 </a>
+  <p>123</p>
 </template>
 <script>
 import { reactive } from "@vue/composition-api";
+console.log(123);
 export default {
   // 父组件传递进来更改该头部组件的属性值
   props: {
